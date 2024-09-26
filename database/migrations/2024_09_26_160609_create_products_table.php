a10e0005e34887ae1dfcff066dbbfd82d7356b19
User 3 <dev.finserveinfotech@gmail.com>
2024-09-26 16:18:21 +0530

    Rishi

--- database/migrations/2024_09_26_160609_create_products_table.php
+++ database/migrations/2024_09_26_160609_create_products_table.php
@@ -13,6 +13,18 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('product_name'); // Product name
+            $table->string('product_code')->unique(); // Unique product code
+            $table->string('hsn_code'); // HSN code
+            $table->text('product_description')->nullable(); // Product description
+            $table->foreignId('cat_id')->nullable()->constrained('categories'); // Category ID
+            $table->foreignId('sub_cat_id')->nullable()->constrained('sub_categories'); // Sub-category ID
+            $table->foreignId('tag_id')->nullable()->constrained('tags'); // Tag ID
+            $table->foreignId('season_id')->nullable()->constrained('seasons'); // Season ID
+            $table->foreignId('brand_id')->nullable()->constrained('brands'); // Brand ID
+            $table->integer('cost_price'); // Cost price
+            $table->integer('sell_price'); // Selling price
+            $table->integer('product_mrp'); // Maximum retail price (MRP)
             $table->timestamps();
         });
     }
