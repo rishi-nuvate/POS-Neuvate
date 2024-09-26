385d8b39b3431d28e89271911ac71afac9da677d
User 3 <dev.finserveinfotech@gmail.com>
2024-09-26 16:18:15 +0530

    Rishi

---
+++ database/migrations/2024_09_26_160712_create_product_variants_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_variants', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->nullable()->constrained('products');
+            $table->string('color')->nullable(); // Color attribute
+            $table->string('size')->nullable(); // Size attribute
+            $table->string('sku')->nullable(); // SKU string
+            $table->string('image')->nullable(); // Image path or URL
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_variants');
+    }
+};
