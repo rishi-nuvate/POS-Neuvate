b28e984f780c7c57d8f6e864dc309fd09dc70f22
User 3 <dev.finserveinfotech@gmail.com>
2024-09-26 11:38:05 +0530

    Rishi

--- app/Http/Controllers/master/basicInfo/TagsController.php
+++ app/Http/Controllers/master/basicInfo/TagsController.php
@@ -79,4 +79,27 @@ class TagsController extends Controller
         }
         return redirect()->route('tags.index')->with('success', 'Tags Deleted.');
     }
+
+    public function getTagData()
+    {
+        $allTags = Tags::all();
+
+//        dd($allBrands);
+        $num = 1;
+        $result = ['data' => []];
+        foreach ($allTags as $tag) {
+
+            $id = $tag->id;
+            $name = $tag->name;
+
+            $action =
+                ' <a href="tags/' . $id . '/edit" title="Edit" class="btn btn-icon btn-label-primary mx-1"><i class="ti ti-edit mx-2 ti-sm"></i></a>
+            <button onclick="daleteTag(' .
+                $tag->id .
+                ')" title="Delete" class="btn btn-icon btn-label-danger mx-1"><i class="ti ti-trash mx-2 ti-sm"></i></button>';
+            array_push($result['data'], [$num, $name, $action]);
+            $num++;
+        }
+        echo json_encode($result);
+    }
 }
